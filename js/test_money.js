aafbc79979c4656972c2141a3c566ae4799096f5
magne <magnello@coders51.com>
2022-09-09 13:48:07 +0200

    feat: addition feature for Money in the same currency

--- js/test_money.js
+++ js/test_money.js
@@ -13,9 +13,24 @@ class Money {
         return new Money(this.amount / divisor, this.currency);
     }
 }
-let fiver = new Money(5, "USD");
-let tenner = new Money(10, "USD");
-(0, assert_1.deepStrictEqual)(fiver.times(2), tenner);
+class Portfolio {
+    constructor() {
+        this.moneys = [];
+    }
+    add(...moneys) {
+        this.moneys = this.moneys.concat(moneys);
+    }
+    evaluate(currency) {
+        let total = this.moneys.reduce((sum, money) => {
+            return sum + money.amount;
+        }, 0);
+        return new Money(total, currency);
+    }
+}
+// -------------------- TESTS --------------------
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+(0, assert_1.deepStrictEqual)(fiveDollars.times(2), tenDollars);
 let tenEuros = new Money(10, "EUR");
 let twentyEuros = new Money(20, "EUR");
 (0, assert_1.deepStrictEqual)(tenEuros.times(2), twentyEuros);
@@ -24,3 +39,7 @@ let originalMoney = new Money(4002, "KRW");
 let actualMoneyAfterDivision = originalMoney.divide(4);
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
 (0, assert_1.deepStrictEqual)(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+let fifteenDollars = new Money(15, "USD");
+let portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+(0, assert_1.deepStrictEqual)(portfolio.evaluate("USD"), fifteenDollars);
--- js/test_money.ts
+++ js/test_money.ts
@@ -1,4 +1,4 @@
-import { strictEqual, deepStrictEqual } from "assert"
+import assert, { strictEqual, deepStrictEqual } from "assert"
 
 class Money {
     amount: number;
@@ -18,14 +18,44 @@ class Money {
     }
 }
 
-let fiver: Money = new Money(5, "USD");
-let tenner = new Money(10, "USD")
-deepStrictEqual(fiver.times(2), tenner);
+class Portfolio {
+    moneys: Money[];
+
+    add(...moneys: Money[]) {
+        this.moneys = this.moneys.concat(moneys);
+    }
+
+    evaluate(currency: "EUR" | "USD" | "KRW") : Money{
+        let total = this.moneys.reduce( (sum, money) => {
+            return sum + money.amount;
+        }, 0);
+        return new Money(total, currency);
+    }
+
+    constructor() {
+        this.moneys = [];
+    }
+}
+
+
+
+// -------------------- TESTS --------------------
+
+let fiveDollars: Money = new Money(5, "USD");
+let tenDollars = new Money(10, "USD")
+deepStrictEqual(fiveDollars.times(2), tenDollars);
+
 let tenEuros: Money = new Money(10, "EUR");
 let twentyEuros = new Money(20, "EUR");
 deepStrictEqual(tenEuros.times(2), twentyEuros);
 deepStrictEqual(twentyEuros.currency, "EUR");
+
 let originalMoney = new Money(4002, "KRW");
 let actualMoneyAfterDivision = originalMoney.divide(4);
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
 deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
+
+let fifteenDollars = new Money(15, "USD");
+let portfolio: Portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars);
